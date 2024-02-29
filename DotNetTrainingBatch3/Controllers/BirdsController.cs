bff7a251f6cf26419f5866375ce6f1f46c06e768
SwanSwanISonFIRE <swamswan943@gmail.com>
2024-02-29 22:54:41 +0630

    [Complete] Bird API

---
+++ DotNetTrainingBatch3/Controllers/BirdsController.cs
@@ -0,0 +1,67 @@
+﻿using DotNetTrainingBatch3.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace DotNetTrainingBatch3.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BirdsController : ControllerBase
+    {
+        private readonly string _url="https://burma-project-ideas.vercel.app/birds";
+        [HttpGet]
+        public async Task <IActionResult> Get()
+        {
+            HttpClient h = new HttpClient();
+            var respone= await h.GetAsync(_url);
+            if (respone.IsSuccessStatusCode)
+            {
+                string json = await respone.Content.ReadAsStringAsync();
+                List<BirdDataModel> birds = JsonConvert.DeserializeObject<List<BirdDataModel>>(json)!;
+
+                List<BirdViewModel> lst = birds.Select(bird => change(bird)).ToList();
+
+                return Ok(lst);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task <IActionResult>  Get(int id)
+        {
+            HttpClient h = new HttpClient();
+            var respone = await h.GetAsync($"{_url}/{id}");
+            if (respone.IsSuccessStatusCode)
+            {
+                string json = await respone.Content.ReadAsStringAsync();
+                BirdDataModel bird = JsonConvert.DeserializeObject<BirdDataModel>(json)!;
+
+                var item = change(bird);
+                return Ok();
+
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        private BirdViewModel change(BirdDataModel bird)
+        {
+            var item = new BirdViewModel
+            {
+                BirdName = bird.BirdMyanmarName,
+                Id = bird.Id,
+                Desp = bird.Description,
+                Photourl = $"https://burma-project-ideas.vercle.app/{bird.ImagePath}"
+
+            };
+            return item;
+        }
+    }
+}
---
+++ DotNetTrainingBatch3/Models/BirdModel.cs
@@ -0,0 +1,25 @@
+﻿namespace DotNetTrainingBatch3.Models
+{
+    public class BirdDataModel
+    {
+        public int Id { get; set; }
+
+        public string BirdMyanmarName { get; set; }
+        public string BirdEnglishName { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+
+    }
+
+    public class BirdViewModel
+    {
+        public int Id { get; set; }
+        public string BirdName { get; set; }
+
+        public string Desp { get; set; }
+        public string Photourl { get; set; }
+
+
+
+    }
+}
